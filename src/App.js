4a7ea2d285775c0e30d54cb6da53ffadbafaa3b8
nigeymc <mckenna.niall@gmail.com>
2020-10-02 11:16:33 +0100

    fiddling around

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-function Header() {
+const Header = () => {
     return (
         <header id="header" className="header">
             <div className="center-logo">
